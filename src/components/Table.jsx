8fc816c4ab611bbc099a731560ba85a5eb839314
Umar Luqman <>
2020-03-05 18:57:50 +0800

    Initial commit 🔥

---
+++ src/components/Table.jsx
@@ -0,0 +1,188 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { memo, useEffect } from "react";
+import { useTable, usePagination } from "react-table";
+import { theme, Text, Flex } from "@chakra-ui/core";
+import { StyledTable, NoDataRow } from "./styles";
+
+const Table = ({
+  columns = [],
+  data = [],
+  fetchData = () => {},
+  loading = false,
+  pageCount: controlledPageCount,
+
+  selectedRows = [],
+  noDataText = "",
+  extraColumnInAction,
+  className
+}) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+
+    state: { pageIndex, pageSize }
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 10 },
+      manualPagination: !!controlledPageCount,
+      pageCount: controlledPageCount,
+      expandSubRows: false
+    },
+
+    usePagination
+  );
+
+  useEffect(() => {
+    fetchData({ pageIndex, pageSize });
+  }, [fetchData, pageIndex, pageSize]);
+
+  console.log("theme", theme);
+
+  const setTheadTextAlign = (items, idx) => {
+    console.log("items", items);
+    return page.length === 0 || idx !== 0
+      ? idx === items.length - 1
+        ? "center"
+        : "left"
+      : "center";
+  };
+
+  const setTbodyTextAlign = (items, idx) => {
+    return idx === 0 ? "center" : items.length - 1 === idx ? "center" : "left";
+  };
+
+  const setTbodyRowBg = row => {
+    return selectedRows.includes(row.original.id) && theme.colors.gray[200];
+  };
+
+  return (
+    <>
+      <StyledTable {...getTableProps()} className={className}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr
+              {...headerGroup.getHeaderGroupProps()}
+              css={{
+                backgroundColor: "#EDF2F77A"
+              }}
+            >
+              {headerGroup.headers.map((column, idx) => (
+                <Text
+                  as="th"
+                  fontSize="sm"
+                  color="gray.500"
+                  {...column.getHeaderProps()}
+                  css={{
+                    textAlign: setTheadTextAlign(headerGroup.headers, idx),
+                    width:
+                      headerGroup.headers.length - 1 === idx &&
+                      !extraColumnInAction
+                        ? "8rem"
+                        : idx === 0
+                        ? "5.5rem"
+                        : "unset",
+                    textTransform: "uppercase",
+                    fontWeight: 400,
+                    padding: "1rem .75rem",
+                    letterSpacing: 1.3
+                  }}
+                >
+                  {column.render("Header")}
+                </Text>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map(
+            row =>
+              prepareRow(row) || (
+                <React.Fragment key={row.index}>
+                  <tr
+                    {...row.getRowProps()}
+                    css={{
+                      backgroundColor: setTbodyRowBg(row),
+                      borderRadius: 2,
+                      fontSize: 14
+                    }}
+                  >
+                    {row.cells.map((cell, idx) => {
+                      return (
+                        <td
+                          {...cell.getCellProps()}
+                          css={{
+                            textAlign: setTbodyTextAlign(row.cells, idx),
+                            padding: "1rem .75rem"
+                          }}
+                        >
+                          {cell.render("Cell")}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                </React.Fragment>
+              )
+          )}
+        </tbody>
+      </StyledTable>
+
+      {/* <Flex justify="space-between">
+        <Text color="gray.600" fontSize="sm">
+          Showing {pageIndex + 1} / {pageOptions.length || 1}
+        </Text>
+        <Flex alignItems="center">
+          <IconButton
+            variant="ghost"
+            variantColor={theme.colors.gray[200]}
+            aria-label="First page"
+            icon={"<<"}
+            onClick={() => gotoPage(0)}
+            disabled={!canPreviousPage}
+          />
+          <IconButton
+            variant="ghost"
+            color={theme.colors.gray[500]}
+            aria-label="Last page"
+            icon="chevron-left"
+            onClick={() => previousPage()}
+            disabled={!canPreviousPage}
+            fontSize={"1.75rem"}
+          />
+          <Circle>{pageIndex + 1}</Circle>
+          <IconButton
+            variant="ghost"
+            fontSize={"1.75rem"}
+            color={theme.colors.gray[500]}
+            aria-label="Last page"
+            icon="chevron-right"
+            onClick={() => nextPage()}
+            disabled={!canNextPage}
+          />
+          <IconButton
+            variant="ghost"
+            variantColor={theme.colors.gray[200]}
+            aria-label="Last page"
+            icon={">>"}
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          />
+        </Flex>
+      </Flex> */}
+    </>
+  );
+};
+
+export default memo(Table);
---
+++ src/components/styles.js
@@ -0,0 +1,58 @@
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+import { theme } from "@chakra-ui/core";
+
+export const Circle = styled.span`
+  height: 34px;
+  width: 34px;
+  font-size: 12px;
+  background-color: ${theme.colors.gray[200]};
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledTable = styled("table")(
+  props =>
+    console.log("props", props) ||
+    css`
+      border: none;
+      /* margin-bottom: 16px; */
+      width: 100%;
+      border-radius: 6px;
+
+      box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+        0 4px 6px -2px rgba(0, 0, 0, 0.05);
+      thead tr:first-of-type {
+        display: table-row;
+      }
+
+      /* th,
+  td {
+    height: 48px;
+    border: none;
+  } */
+
+      td {
+        border-top-width: 1.2px;
+        border-top-style: solid;
+        background-color: transparent;
+        border-top-color: ${theme.colors.gray[200]};
+        border-bottom-width: 1px;
+        border-bottom-style: solid;
+        border-bottom-color: ${theme.colors.gray[200]};
+      }
+
+      &.project-table td:first-of-type {
+        text-align: left;
+      }
+    `
+);
+
+export const NoDataRow = styled.td`
+  color: ${theme.colors.black[50]};
+  font-size: 14px;
+  font-weight: 400;
+  padding: 1rem;
+`;
