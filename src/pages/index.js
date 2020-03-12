1679cb69304d8fdda3769e83c4d7d7908659231e
Umar Luqman <>
2020-03-12 18:48:01 +0800

    Added navigation page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,46 @@
 /** @jsx jsx */
-import { Flex } from "@chakra-ui/core";
+import { Flex, Text, Button } from "@chakra-ui/core";
 import { jsx } from "@emotion/core";
 import { withTheme } from "emotion-theming";
+import Link from "next/link";
 
 const Index = () => {
-  return <Flex>Hi</Flex>;
+  return (
+    <Flex
+      direction="column"
+      minHeight="100vh"
+      w="100%"
+      align="center"
+      justify="center"
+      bg="rgb(37, 47, 63)"
+      p={4}
+    >
+      <Text fontSize="xl" fontWeight={600} color="white" pb={2}>
+        {" "}
+        Hi, Welcome to Showcase!
+      </Text>
+
+      <Text color="white" textAlign="center">
+        Collection of UI made up with React & 👩🏻‍🎤 Emotion
+      </Text>
+      <Flex w="100%" maxWidth="50%" justify="center" mt={8} flexWrap="wrap">
+        <Link href="/dashboard">
+          <Button variant="ghost" variantColor="pink">
+            Dashboard
+          </Button>
+        </Link>
+
+        <Button variant="ghost" variantColor="pink" disabled>
+          Landing Page
+        </Button>
+        <Link href="/complex-form">
+          <Button variant="ghost" variantColor="pink">
+            Complex Form
+          </Button>
+        </Link>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default withTheme(Index);
